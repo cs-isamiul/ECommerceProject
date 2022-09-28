3f46ddb52bb102fd5f9d4f99bad9320dcdcbe58c
Benny Naftali <naftali.1@buckeyemail.osu.edu>
2022-09-28 11:40:16 -0400

    testing commit on a different env

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-
-//import './App.css'; 
+//import './App.css';
 //TODO: add CSS styling
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Catalog from './components/catalogRoutes/Catalog';
-import Home from './components/Home';
-import Error from './components/Error';
-import Cart from './components/shoppingCart/Cart';
+import Catalog from "./components/catalogRoutes/Catalog";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import Cart from "./components/shoppingCart/Cart";
 
 function App() {
   return (
-      <div>
-        <Router>
-          <Routes>
-           <Route exact path='/' element={<Home/>} />
-           <Route path='/catalog' element={<Catalog/>} />
-           <Route path='/cart' element={<Cart/>} />
-           <Route path='*' element={<Error/>} />
-          </Routes>
-        </Router>
-      </div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;

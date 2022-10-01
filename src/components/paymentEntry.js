d3a38a23472efc5148e4cf0dde9158dd17ebc21f
Chien-Heng,Yu <51120416+Hansloth@users.noreply.github.com>
2022-10-01 17:00:17 -0400

    first commit

--- src/components/paymentEntry.js
+++ src/components/paymentEntry.js
@@ -1,6 +1,101 @@
 import React from "react";
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Payment() {
-  return <h2>Billing Information</h2>;
+  const [payState, setPayState] = useState({
+    paymentFirstName: "",
+    paymentLastName: "",
+    paymentCardNum: "",
+    paymentCardCVC:"",
+    paymentCardYear:"",
+    paymentCardMonth:"",
+  });
+  const navigate = useNavigate();
+
+
+  const onChange = (e) => {
+    /*
+      used to set state of 
+
+      paymentFirstName
+      paymentLastName
+      paymentCardNum
+      paymentCardCVC
+      paymentCardYear
+      paymentCardMonth
+    */
+    const { name, value } = e.target;
+    setPayState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+
+  const handleSubmit = (e) =>{
+    navigate('/cart/shipping',{payState: payState, setPayState: setPayState});
+    // history.push('/cart/shipping');
+  }
+
+  return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <h1>Payment Information</h1>
+
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentFirstName"
+        placeholder="First Name"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentLastName"
+        placeholder="Last Name"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentCardNum"
+        placeholder="Credit Card Number"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentCardCVC"
+        placeholder="Card  CVC"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentCardYear"
+        placeholder="Card Expire Year"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      <input 
+        className = 'paymentEntry-font'
+        type="text" 
+        name = "paymentCardMonth"
+        placeholder="Card Expire Month"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+      {/* <input type = "button" className = 'bth' value='Pay'/> */}
+      <button>Pay</button>
+    </form>
+  </div>);
+ 
   //TODO: develop entry for the collection of billing information before placing an order.
 }
--- src/components/shippingEntry.js
+++ src/components/shippingEntry.js
@@ -1,6 +1,126 @@
 import React from "react";
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Shipping() {
-  return <h2>Shipping Information</h2>;
+export default function Shipping(payState, paySetState) {
+  const [shipState, shipSetState] = useState({
+    shippingFirstName: "",
+    shippingLastName: "",
+    shippingPhoneNumber:"",
+    shippingAddressOne:"",
+    shippingAddressTwo:"",
+    shippingCity:"",
+    shippingState:"",
+    shippingZip:"",
+  });
+
+
+  const onChange = (e) => {
+    /*
+      used to set state of 
+
+      shippingFirstName
+      shippingLastName
+      shippingPhoneNumber
+    */
+    const { name, value } = e.target;
+    shipSetState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+  // const navigate = useNavigate();
+
+  // const handleSubmit = (e) =>{
+  //   navigate('/cart/shipping', {state: state, setState: setState});
+  // }
+
+  return (
+  <div>
+    <form >
+      <h1>Shipping Information</h1>
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingFirstName"
+        placeholder="First Name"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingLastName"
+        placeholder="Last Name"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingPhoneNumber"
+        placeholder="Phone Number"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingAddressOne"
+        placeholder="Address 1"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingAddressTwo"
+        placeholder="Address 2"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingCity"
+        placeholder="City"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingState"
+        placeholder="State"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+      <input 
+        className = 'shippingEntey-font'
+        type="text" 
+        name = "shippingZip"
+        placeholder="Zip Code"
+        autoComplete ={false}
+        onChange={onChange}
+      />
+
+
+      <button>Place Order</button>
+
+    </form>
+  </div>);
+  
   //TODO: develop entry for the collection of shipping information before placing an order.
 }
+
+//alert or consol log

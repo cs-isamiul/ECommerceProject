aac6727dc081bf7303930efecd461df63f748802
Samiul Islam <cs.isamiul@gmail.com>
2022-10-01 17:55:42 -0400

    Created an initial confirmation page with dummy variables

--- src/components/confirmation.js
+++ src/components/confirmation.js
@@ -1,6 +1,22 @@
 import React from "react";
 
-export default function OrderConfirm() {
-  return <h2>Order Confirmation</h2>;
-  //TODO: develop order confirmation to display when users place an order.
+const orderInformation = {
+  id: 43798,
+  currency: "$",
+  total: "$378.45",
+  estimatedDeliveryDateMin: "October 2, 2022",
+  estimatedDeliveryDateMax: "December 9, 2022",
 }
+
+function OrderConfirm() {
+  return (
+    <>
+      <h1>Your Order Has Been Confirmed! Thank you for shopping at "Get Connected!"</h1>
+      <h2>Your order id is: {orderInformation.id}</h2>
+      <h2>Your order total is: {orderInformation.total}</h2>
+      <h2>Your estimated deliver date as of now is: {orderInformation.estimatedDeliveryDateMin}-{orderInformation.estimatedDeliveryDateMax}</h2>
+    </>
+  );
+}
+
+export default OrderConfirm;

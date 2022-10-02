34891775a5ae8f8719efa681b1c8c06fc0c32ede
Benny Naftali <naftali.1@buckeyemail.osu.edu>
2022-10-01 21:48:20 -0400

    added shop interface, item catalog, shopping cart feature, and configured invalid route catching.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-//import './App.css';
+//import "./App.css";
+//import "./style.css";
+
 //TODO: add CSS styling
 import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Shop from "./components//shop";
+import Shop from "./components/shop";
 import Home from "./components/home";
 import Error from "./components/error";
 import Cart from "./components/shoppingCart";
@@ -13,22 +15,26 @@ import Shipping from "./components/shippingEntry";
 import OrderSummary from "./components/viewOrder";
 import OrderConfirm from "./components/confirmation";
 
+// Bootstrap CSS
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/catalog" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/cart/purchase" element={<Purchase />} />
-          <Route path="/cart/shipping" element={<Shipping />} />
-          <Route path="/cart/orderSummary" element={<OrderSummary />} />
-          <Route path="/cart/confirmation" element={<OrderConfirm />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/catalog" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart/purchase" element={<Purchase />} />
+        <Route path="/cart/shipping" element={<Shipping />} />
+        <Route path="/cart/orderSummary" element={<OrderSummary />} />
+        <Route path="/cart/confirmation" element={<OrderConfirm />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 

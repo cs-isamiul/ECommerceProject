c6d8e242d918dc1049a100e6a9a114f8e0ed554e
Chien-Heng,Yu <51120416+Hansloth@users.noreply.github.com>
2022-10-02 12:54:51 -0400

    second commit

--- src/components/paymentEntry.js
+++ src/components/paymentEntry.js
@@ -1,20 +1,19 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import "bootstrap/dist/css/bootstrap.min.css"
+import { Form, Button, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Payment() {
   const [payState, setPayState] = useState({
     paymentFirstName: "",
     paymentLastName: "",
     paymentCardNum: "",
-    paymentCardCVC:"",
-    paymentCardYear:"",
-    paymentCardMonth:"",
+    paymentCardCVC: "",
+    paymentCardYear: "",
+    paymentCardMonth: "",
   });
   const navigate = useNavigate();
-
 
   const onChange = (e) => {
     /*
@@ -34,70 +33,107 @@ export default function Payment() {
     }));
   };
 
-
-
-  const handleSubmit = (e) =>{
-    navigate('/cart/shipping',{payState: payState, setPayState: setPayState});
+  const handleSubmit = (e) => {
+    navigate("/cart/shipping", {
+      payState: payState,
+      setPayState: setPayState,
+    });
     // history.push('/cart/shipping');
-  }
+  };
 
   return (
-  <div>
-    <form onSubmit={handleSubmit}>
-      <h1>Payment Information</h1>
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <h1>Payment Information</h1>
 
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentFirstName"
-        placeholder="First Name"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentLastName"
-        placeholder="Last Name"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentCardNum"
-        placeholder="Credit Card Number"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentCardCVC"
-        placeholder="Card  CVC"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentCardYear"
-        placeholder="Card Expire Year"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      <input 
-        className = 'paymentEntry-font'
-        type="text" 
-        name = "paymentCardMonth"
-        placeholder="Card Expire Month"
-        autoComplete ={false}
-        onChange={onChange}
-      />
-      
-      <button>Pay</button>
-    </form>
-  </div>);
- 
-  //TODO: develop entry for the collection of billing information before placing an order.
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentFirstName"
+              placeholder="First Name"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentLastName"
+              placeholder="Last Name"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Credit Card Number</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentCardNum"
+              placeholder="xxxx-xxxx-xxxx-xxxx"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Card CVC</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentCardCVC"
+              placeholder="xxx"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Card Expire Year</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentCardYear"
+              placeholder="xxxx (1950-2030)"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Card Expire Month</Form.Label>
+            <Form.Control
+              className="paymentEntry-font"
+              type="text"
+              name="paymentCardMonth"
+              placeholder="01-12"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
+
+        <button>Pay</button>
+      </Form>
+    </div>
+  );
+
+  //TODO: develop entry for the collection of billing inFormation before placing an order.
 }
--- src/components/shippingEntry.js
+++ src/components/shippingEntry.js
@@ -1,17 +1,18 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 export default function Shipping(payState, paySetState) {
   const [shipState, shipSetState] = useState({
     shippingFirstName: "",
     shippingLastName: "",
-    shippingPhoneNumber:"",
-    shippingAddressOne:"",
-    shippingAddressTwo:"",
-    shippingCity:"",
-    shippingState:"",
-    shippingZip:"",
+    shippingPhoneNumber: "",
+    shippingAddressOne: "",
+    shippingAddressTwo: "",
+    shippingCity: "",
+    shippingState: "",
+    shippingZip: "",
   });
 
   const location = useLocation();
@@ -31,7 +32,6 @@ export default function Shipping(payState, paySetState) {
     }));
   };
 
-
   // const navigate = useNavigate();
 
   // const handleSubmit = (e) =>{
@@ -39,88 +39,127 @@ export default function Shipping(payState, paySetState) {
   // }
   console.log(location);
   return (
-  <div>
-    <form >
-      <h1>Shipping Information</h1>
+    <div>
+      <Form>
+        <h1>Shipping Information</h1>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingFirstName"
-        placeholder="First Name"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingFirstName"
+              placeholder="First Name"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingLastName"
-        placeholder="Last Name"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingLastName"
+              placeholder="Last Name"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingPhoneNumber"
-        placeholder="Phone Number"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingPhoneNumber"
+              placeholder="*** - *** ****"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingAddressOne"
-        placeholder="Address 1"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+          <Form.Label>Address 1</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingAddressOne"
+              placeholder="Address 1"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingAddressTwo"
-        placeholder="Address 2"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+          <Form.Label>Address 2</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingAddressTwo"
+              placeholder="Address 2"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingCity"
-        placeholder="City"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+          <Form.Label>City</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingCity"
+              placeholder="City"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingState"
-        placeholder="State"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+          <Form.Label>State</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingState"
+              placeholder="State"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
-      <input 
-        className = 'shippingEntey-font'
-        type="text" 
-        name = "shippingZip"
-        placeholder="Zip Code"
-        autoComplete ={false}
-        onChange={onChange}
-      />
+        <Col xs={4}>
+          <Form.Group>
+          <Form.Label>Zip Code</Form.Label>
+            <Form.Control
+              className="shippingEntey-font"
+              type="text"
+              name="shippingZip"
+              placeholder="Zip Code"
+              autoComplete={false}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Col>
 
+        <button>Place Order</button>
+      </Form>
+    </div>
+  );
 
-      <button>Place Order</button>
-
-    </form>
-  </div>);
-  
   //TODO: develop entry for the collection of shipping information before placing an order.
 }
 

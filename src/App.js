bd5a0f1dd5c55713bd7c7ac11a937ec5edb56df4
Benny Naftali <naftali.1@buckeyemail.osu.edu>
2022-10-02 20:40:47 -0400

    fixed a typos in routing file

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Shop from "./components/shop";
-import Home from "./components/Home";
+import Home from "./components/home";
 import Error from "./components/Error";
 import Cart from "./components/shoppingCart";
 import Purchase from "./components/paymentEntry";
 import Shipping from "./components/shippingEntry";
-import OrderSummary from "./components/viewOrders";
+import OrderSummary from "./components/viewOrder";
 import OrderConfirm from "./components/confirmation";
 
 // Bootstrap CSS

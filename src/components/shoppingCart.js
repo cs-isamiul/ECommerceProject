d734d608033b0f69902d438bd37345bd4005f7c8
Stephen Southard <stephen.southard.ss@gmail.com>
2022-10-03 17:53:36 -0400

    Refactor for readability

--- src/components/shoppingCart.js
+++ src/components/shoppingCart.js
@@ -1,28 +1,17 @@
-import { BsCartFill } from "react-icons/bs";
-import ItemsCount from "./itemsCount";
 import React, { useState } from "react";
-import { useNavigate, Link, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import Badge from "react-bootstrap/Badge";
 import CartItem from "./CartItem";
+import StandardNavbar from "./standardNavbar"
 
 export default function Cart() {
   const location = useLocation();
   const [cart, setCart] = useState(location.state.cart);
+  const navigate = useNavigate();
 
   const deleteFromCart = (phoneItem) => {
-    setCart(
-      cart => cart.filter(phone => {return phone.id !== phoneItem})
-    )
-  };
-
-  //navigate to 'cart/purchase' and pass cart content.
-  const navigate = useNavigate();
-  const handleCart = () => {
-    navigate("/cart", { state: { cart: cart } });
+    setCart(cart => cart.filter(phone => {return phone.id !== phoneItem}))
   };
   const handleCheckout = () => {
     navigate("/cart/purchase", { state: { cart: cart } });
@@ -30,56 +19,25 @@ export default function Cart() {
 
   return (
     <div className="shop">
-      <Navbar bg="light" expand="lg" sticky="top">
-        <Container fluid>
-          <Navbar.Brand href="#">E-shop</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-
-              <Link to="/catalog" className="nav-link">
-                Shop
-              </Link>
-
-              <Nav.Link href="#" disabled>
-                About
-              </Nav.Link>
-            </Nav>
-            <Button variant="outline-dark" onClick={handleCart}>
-              <BsCartFill style={{ verticalAlign: "sub" }} /> Cart
-              <Badge pill bg="dark" style={{ marginLeft: ".2rem" }}>
-                <ItemsCount cart={cart} />
-              </Badge>
-            </Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
+      <StandardNavbar />
       <h1>Your Cart</h1>
-
-      {cart.length > 0 ?           
-          <React.Fragment>
-            <Button variant="outline-dark" onClick={handleCheckout}>
-              Proceed to Checkout
-              <Badge pill bg="dark" style={{ marginLeft: ".2rem" }}>
-              </Badge>
-            </Button>
-            <section className="shoplist">
-              {cart.map((cart) => {
-                return ( 
-                  <CartItem key={cart.id} phoneItem={cart} deleteFromCart={deleteFromCart}></CartItem>
-                );
-              })}
-            </section>
-          </React.Fragment> : <h3>Cart is empty!</h3>
-      }
+      {cart.length > 0 ? ProceedToCheckoutButton() : <h3>Cart is empty!</h3>}
     </div>
   );
+
+  function ProceedToCheckoutButton() {
+    return (
+      <React.Fragment>
+        <Button variant="outline-dark" onClick={handleCheckout}>
+          Proceed to Checkout
+          <Badge pill bg="dark" style={{ marginLeft: ".2rem" }} />
+        </Button>
+        <section className="shoplist">
+          {cart.map((cart) => {
+            return <CartItem key={cart.id} phoneItem={cart} deleteFromCart={deleteFromCart}></CartItem>;
+          })}
+        </section>
+      </React.Fragment>
+    );
+  }
 }

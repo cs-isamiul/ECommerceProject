7b68eda74c17b1a0c0beacb3f80d45b02461ae3e
Samiul Islam <cs.isamiul@gmail.com>
2022-10-05 18:52:20 -0400

    Example of rendering a component on every route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Purchase from "./components/paymentEntry";
 import Shipping from "./components/shippingEntry";
 import OrderSummary from "./components/viewOrders";
 import OrderConfirm from "./components/confirmation";
+import StandardNavbar from "./components/standardNavbar";
 
 // Bootstrap CSS
 import "./style.css";
@@ -23,18 +24,19 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Shop />} /> {/* Was home */}
-        <Route path="/catalog" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/cart/purchase" element={<Purchase />} />
-        <Route path="/cart/shipping" element={<Shipping />} />
-        <Route path="/cart/orderSummary" element={<OrderConfirm />} /> {/* Was orderSummary, but confirm does same thing right now*/}
-        <Route path="/cart/confirmation" element={<OrderConfirm />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+      <Router>
+        {/* <StandardNavbar /> */}
+            <Routes>
+              <Route exact path="/" element={<Shop />} /> {/* Was home */}
+              <Route path="/catalog" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/cart/purchase" element={<Purchase />} />
+              <Route path="/cart/shipping" element={<Shipping />} />
+              <Route path="/cart/orderSummary" element={<OrderConfirm />} /> {/* Was orderSummary, but confirm does same thing right now*/}
+              <Route path="/cart/confirmation" element={<OrderConfirm />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+      </Router>
   );
 }
 

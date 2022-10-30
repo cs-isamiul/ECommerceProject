6cd046765110820007a453b35d17241f2ecb3f55
Chien-Heng,Yu <51120416+Hansloth@users.noreply.github.com>
2022-10-30 09:57:46 -0400

    inventory have bug connecting to db

--- NodeJS-Backend/controllers/inventory.js
+++ NodeJS-Backend/controllers/inventory.js
@@ -1,5 +1,8 @@
 const inventory = require("../data/phoneSpecifications.json");
 const asyncWrapper = require("../middleware/async");
+const Inventory = require("../models/inventorySchema")
+const mongoose = require('mongoose');
+
 
 const getAllInventory = asyncWrapper(async(req, res)=>{
     res.send(inventory.filter((phone) => phone.invQty > 0));
@@ -7,9 +10,42 @@ const getAllInventory = asyncWrapper(async(req, res)=>{
 
 //make a request to update, put 
 const updataInventoryDB = asyncWrapper(async(req, res)=>{
-    const {id, qty} = req.body;
-    res.status(200).json({message:"hi"});
-})
+
+
+    //this program from line 15 to 24 is used to test if db data is retrievable 
+    var TargetInventoryQty;
+    TargetInventoryQty = Inventory.find()
+        .then((result) =>{
+            res.send(result)
+            console.log(result) 
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+
+
+    // //req will be {id, quantity}
+    // const {id, qty} = req.body;
+    // var TargetInventoryQty;
+    // var newQty;
+    
+    // TargetInventory = await Inventory.find({"id": id})
+    //     .then((result) => {
+    //         TargetQuantity= TargetInventory.invQty; //gets the target inventory's quantity in db 
+    //         console.log("origin value " +  TargetInventoryQty); //for debug purpose
+    //         newQty = TargetInventoryQty - qty;  //new quantity will be (target inventory's quantity in db) - the qty that this req wants to delete
+    //         console.log("newQty value " +  newQty);  //for debug purpose
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err);
+    //     });
+    // await Inventory.findByIdAndUpdate(TargetInventory.id, {
+    //     invQty: newQty
+    // });
+    // res.send('Item Upadated!');
+
+
+});
 
 const getSingleItem = asyncWrapper(async(req, res)=>{
     const {id, model} = req.body;
--- NodeJS-Backend/routes/routing.js
+++ NodeJS-Backend/routes/routing.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const {getTest, createOrder} = require("../controllers/processOrder");
-const {getAllInventory, getSingleItem} = require("../controllers/inventory");
+const {getAllInventory, getSingleItem, updataInventoryDB} = require("../controllers/inventory");
 
-router.route("/processorder").get(getTest).post(createOrder);
-router.route("/inventory").get(getAllInventory);
-router.route("/inventory/single").get(getSingleItem);
+router.route("/processorder")
+    .get(getTest)
+    .post(createOrder);
+router.route("/inventory")
+    .get(getAllInventory)
+    .put(updataInventoryDB);
+router.route("/inventory/single")
+    .get(getSingleItem);
 
 module.exports = router;

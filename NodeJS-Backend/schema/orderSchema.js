74e7170e39613ee5941c204edcf539537096662c
Samiul Islam <cs.isamiul@gmail.com>
2022-11-20 10:11:18 -0500

    Updated order, payment and shipping schema
    Schema's were updated, TODO fix orderProcess controller to properly use these schema's so we can remove extra if's

--- NodeJS-Backend/schema/orderSchema.js
+++ NodeJS-Backend/schema/orderSchema.js
@@ -1,40 +1,26 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-    Order: {
-        orderTotal: {
-            type: String,
-            required:[true],
-            trim:true,
-        },
-        orderPlacedOn: {
-            type: String,
-            required:[true],
-            trim:true,
-        },
-        estimatedDeliveryDateMax: {
-            type: String,
-            required:[true],
-            trim:true,
-        },
-        estimatedDeliveryDateMin: {
-            type: String,
-            required:[true],
-            trim:true,
-        },
+    orderTotal: {
+        type:String,
+        required:[true, "Must provide an order total"]
     },
-    Items: {
-        itemIds: {
-            type: Array,    // Store array of item id
-            required:[true], 
-            trim:true,
-        },
-        itemQty: {
-            type: Array,    // Store array of item qty associated with item id above
-            required:[true], 
-            trim:true,
-        }
+    orderPlacedOn: {
+        type:String,
+        required:[true, "Must provide date"]
     },
+    items: [{
+        id:String,
+        qty:String
+    }],
+    payment:{
+        type:ObjectId,
+        required:[true, "Must provide payment refrence"]
+    },
+    shipping:{
+        type:ObjectId,
+        required:[true, "Must provide payment refrence"]
+    }
 });
 
 module.exports = OrderSchema;
